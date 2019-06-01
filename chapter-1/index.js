561fd11538f6208bda56e77719539570c15cf847
Steven Wiggins <stevenwiggins@stockx.com>
2019-06-01 13:43:09 -0400

    fix eslint errors
    a console log error remains but ignoring for now

--- chapter-1/index.js
+++ chapter-1/index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+const port = 8080;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
